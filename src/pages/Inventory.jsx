5b7582fd5b1be7fde23ef1bc34c5c2467e673296
Charles <adeakinyemi129@gmail.com>
2025-09-28 20:56:28 +0100

    updated inventory

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -211,7 +211,7 @@ export default function Inventory() {
     setOpen(true);
   };
 
-  const categories = ["Phones","Laptops","Gadgets", "Fashion", "Medicine", "Furniture", "Restaurant"];
+  const categories = ["Phones","Laptops","Gadgets", "Fashion", "Medicine", "Furniture", "Restaurant", "Journal"];
 
   const openEdit = (item) => {
     setMode("edit");
