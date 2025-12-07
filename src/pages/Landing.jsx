65881ad510f93e2bf64f6bd36c33188f81ae6221
Charles <adeakinyemi129@gmail.com>
2025-12-07 21:47:22 +0100

    people slider

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,12 +5,14 @@ import Features from '../components/sections/Features'
 import Footer from '../components/Footer'
 import Pricing from '../components/sections/Pricing'
 import Testimonials from '../components/sections/Testimonials'
+import PeopleSlider from '../components/sections/PeopleSlider'
 
 export default function Landing(){
   return (
     <div>
       <Navbar />
       <Hero />
+      <PeopleSlider/>
       <Features />
       <Pricing/>
       <Testimonials/>
