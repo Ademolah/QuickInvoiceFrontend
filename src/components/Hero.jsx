b0d4b5e9af19b01cbf1382e5f2a3f6c896ebbc23
Charles <adeakinyemi129@gmail.com>
2026-01-16 20:33:24 +0100

    fixed Hero x2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 // import React from "react";
 import { Link } from "react-router-dom";
 
@@ -73,7 +75,7 @@ export default function Hero({
   };
 
 
-  const invoiceAmount = useMotionValue(0);
+const invoiceAmount = useMotionValue(0);
 const formattedAmount = useTransform(invoiceAmount, (v) =>
   `₦ ${Math.floor(v).toLocaleString()}`
 );
